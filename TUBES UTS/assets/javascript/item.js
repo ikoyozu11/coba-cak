c602b04ba715f36dffe7881d65f6f87083d35f22
KanonFueee34 <aliputra3524@gmail.com>
2024-04-21 22:22:01 +0700

    updates

--- TUBES UTS/assets/javascript/item.js
+++ TUBES UTS/assets/javascript/item.js
@@ -1,34 +1,49 @@
-const endpoint = "https://pokeapi.co/api/v2/item?limit=24";
-fetch(endpoint)
-  .then((response) => response.json())
-  .then((data) => {
-    let result = data.results;
-    let cards = ""; // Variabel untuk menampung hasil dari showCards
-    result.forEach((item) => {
-      let itemUrl = item.url;
-      fetch(itemUrl)
-        .then((response) => response.json())
-        .then((Url) => {
-          let itemImage = Url.sprites.default;
-          console.log(itemImage);
-          let itemName = Url.name;
-          cards += showCards(itemName, itemImage); // Tambahkan hasil dari showCards ke dalam variabel cards
+let currentPage = 1;
+const itemsPerPage = 56;
+
+function fetchItems(page) {
+  const endpoint = `https://pokeapi.co/api/v2/item?limit=${itemsPerPage}&offset=${(page - 1) * itemsPerPage}`;
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      let result = data.results;
+
+      // Fetch category information for each item
+      Promise.all(result.map(item => fetch(item.url).then(response => response.json())))
+        .then(items => {
+          // Sort items based on their categories
+          items.sort((a, b) => a.category.name.localeCompare(b.category.name));
+
+          let cards = ""; // Variable to store the generated cards
+          items.forEach(item => {
+            let itemImage = item.sprites.default;
+            let itemName = item.name;
+            cards += showCards(itemName, itemImage); // Add the card to the cards variable
+          });
+
           const pokemonContainer = document.querySelector("#pokemon-list");
-          pokemonContainer.innerHTML = cards;
+          pokemonContainer.innerHTML = cards; // Update the HTML content with the generated cards
         });
     });
-  });
+}
+
+// Function to load the next page of items
+function loadNextPage() {
+  currentPage++;
+  fetchItems(currentPage);
+}
+
+// Initial load of items
+fetchItems(currentPage);
 
 function showCards(itemName, itemImage) {
   return `
-  <a href="#"
-  <div class="col-lg-2 col-md-3 col-sm-4 mb-4">
-    <div class="card">
-      <img src="${itemImage}" class="card-img-top mx-auto" alt="${itemName}" style="width: 150px">
-      <div class="card-body">
-      <h5 class="text-center card-title">${itemName}</h5>
+  <a href="#">
+      <div class="card">
+        <img src="${itemImage}" class="card-img-top mx-auto" alt="${itemName}" style="width: 150px">
+        <div class="card-body">
+          <h5 class="text-center card-title">${itemName}</h5>
+        </div>
       </div>
-    </div>
-  </div></a> 
-  `;
+  </a>`;
 }
